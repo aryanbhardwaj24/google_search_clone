c8629370fb4dc89aff696bca8ad84472bdfdde48
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-02 04:03:45 +0530

    add ResponsiveLayoutScreen in order to vary the layout of the app to be either mobileScreenLayout or webScreenLayout depending on the width of the device screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_search_clone/responsive/responsive_layout_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text('Google search clone!'),
+      home: ResponsiveLayoutScreen(
+          mobileScreenLayout: mobileScreenLayout,
+          webScreenLayout: webScreenLayout),
     );
   }
 }
