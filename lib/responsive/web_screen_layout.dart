60e33d2e4a520ec21f3f5f0c045e1cc473d69378
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-12 14:36:46 +0530

    update the main screen body to include the Google logo

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_search_clone/colors.dart';
+import 'package:google_search_clone/widgets/search.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: backgroundColor,
@@ -63,9 +67,29 @@ class WebScreenLayout extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Hello, this is from WebScreenLayout!',
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 5,
+          right: 5,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.height * 0.25,
+            ),
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Search(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/search.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  const Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/images/google-logo.png',
+            height: size.height * 0.15,
+          ),
+        ),
+      ],
+    );
+  }
+}
