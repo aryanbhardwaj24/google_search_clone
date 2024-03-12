6086733953372e1f130ab03b1d3756951c4bbd70
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-12 14:11:09 +0530

    design the main screen appBar

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_search_clone/colors.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              colorFilter:
+                  const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0).copyWith(
+              right: 10.0,
+            ),
+            child: MaterialButton(
+              color: const Color(0xff1A73EB),
+              onPressed: () {},
+              child: const Text(
+                'Sign in',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text(
           'Hello, this is from WebScreenLayout!',
         ),
