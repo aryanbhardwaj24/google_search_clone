20338c19f1e87e023572534793d55554b35c5d37
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-13 03:06:40 +0530

    replace the build function of mobileScreenLayout by that of webScreenLayout and import the necessary packages in order to be consistent with the UI for the common stuffs between mobile and web screen layouts

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,15 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_search_clone/colors.dart';
+import 'package:google_search_clone/widgets/search.dart';
+import 'package:google_search_clone/widgets/translation_buttons.dart';
+import 'package:google_search_clone/widgets/web/search_buttons.dart';
+import 'package:google_search_clone/widgets/web/web_footer.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello, this is from MobileScreenLayout!',
-        ),
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              colorFilter:
+                  const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0).copyWith(
+              right: 10.0,
+            ),
+            child: MaterialButton(
+              color: const Color(0xff1A73EB),
+              onPressed: () {},
+              child: const Text(
+                'Sign in',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.25,
+          ),
+          const Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Search(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SearchButtons(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TranslationButtons(),
+                  ],
+                ),
+                WebFooter(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
