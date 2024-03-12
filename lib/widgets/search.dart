6c99fdf811fc5e5ad1d0efc7ca95fd7acef87a39
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-12 22:34:00 +0530

    add the search field to the main screen body

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_search_clone/colors.dart';
 
 class Search extends StatelessWidget {
   const Search({super.key});
@@ -15,6 +17,42 @@ class Search extends StatelessWidget {
           child: Image.asset(
             'assets/images/google-logo.png',
             height: size.height * 0.15,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          width: size.width * 0.45,
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: searchBorder,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    30,
+                  ),
+                ),
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'assets/images/search-icon.svg',
+                  colorFilter:
+                      const ColorFilter.mode(searchColor, BlendMode.srcIn),
+                ),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'assets/images/mic-icon.svg',
+                  colorFilter:
+                      const ColorFilter.mode(searchColor, BlendMode.srcIn),
+                ),
+              ),
+            ),
           ),
         ),
       ],
