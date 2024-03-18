8ef15fd4e2d2c1e1f6a5402dd7b79d64bad79cd0
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-18 18:00:23 +0530

    add search tabs to the search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_search_clone/widgets/search_header.dart';
+import 'package:google_search_clone/widgets/search_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -15,6 +16,10 @@ class SearchScreen extends StatelessWidget {
             //Web Header
             SearchHeader(),
             //tabs for images, news, ...etc.
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SearchTabs(),
+            ),
             //search results (including the time taken to fetch the results)
             //pagination buttons
           ],
---
+++ lib/widgets/search_tabs.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_search_clone/widgets/search_tab.dart';
+
+class SearchTabs extends StatelessWidget {
+  const SearchTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 55,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SearchTab(
+              isActive: true,
+              title: 'All',
+              icon: Icons.search,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            SearchTab(
+              title: 'Images',
+              icon: Icons.image,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            SearchTab(
+              title: 'Map',
+              icon: Icons.map,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            SearchTab(
+              title: 'News',
+              icon: Icons.newspaper,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            SearchTab(
+              title: 'Shopping',
+              icon: Icons.shop,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            SearchTab(
+              title: 'More',
+              icon: Icons.more_vert,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
