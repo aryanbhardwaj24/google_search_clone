9caf7971be582efd35d52935b9d87852a9d57fb7
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-20 03:37:31 +0530

    update the bool isDummyData variable to be false in order to fetch data through API calling

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  bool isDummyData = true;
+  bool isDummyData = false;
 
   Future<Map<String, dynamic>> fetchData(
       {required String queryTerm, String start = '0'}) async {
